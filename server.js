7206714e6a844180328af0ed4cfc6ab1bba646a3
behindScene <hassansagheer099@gmail.com>
2023-01-29 13:33:51 +0500

    add comment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//hello world
 const express = require('express');
 const cors = require('cors');
 
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/api/v1/products', require('./routes/product'));
+app.use('/api/v1/products', reqgituire('./routes/product'));
 app.use('/api/v1/users', require('./routes/user'));
 
 app.get('/', (req, res, next) => {
